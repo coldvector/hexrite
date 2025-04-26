2882256d26240aac8e7c92fa0d2b2fb6f5d0131f
Bhavyai Gupta <zbhavyai@gmail.com>
2025-04-26 12:50:54 -0600

    use flyway for dev mode for development

---
+++ backend/src/main/resources/db/migration/dev/V100__Connection.sql
@@ -0,0 +1,33 @@
+INSERT INTO hexrite.connection (
+        "id",
+        "name",
+        "description",
+        "type",
+        "base_url",
+        "api_key",
+        "created_at",
+        "updated_at",
+        "enabled"
+    )
+VALUES (
+        '33e8015c-d306-4cf1-a983-a0c248ba2809',
+        'Ollama-1',
+        'Workstation Ollama',
+        'OLLAMA',
+        'http://testnet2:11434',
+        '',
+        '2025-04-20 18:19:15.10535+00',
+        '2025-04-20 18:19:15.10535+00',
+        true
+    ),
+    (
+        'c683af6f-7968-460d-bf97-09e9aa5c5888',
+        'Ollama-2',
+        'Homestation Ollama',
+        'OLLAMA',
+        'http://127.0.0.1:11434',
+        '',
+        '2025-04-23 18:19:15.10535+00',
+        '2025-04-23 18:19:15.10535+00',
+        false
+    );
