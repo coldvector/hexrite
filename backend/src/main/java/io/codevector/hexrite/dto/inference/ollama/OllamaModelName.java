d507238c0e7242cf0cbeeecf3e4f305df9f1af6f
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-03 09:45:06 -0600

    remove unused dto

--- backend/src/main/java/io/codevector/hexrite/dto/inference/ollama/OllamaModelName.java
+++
@@ -1,9 +0,0 @@
-package io.codevector.hexrite.dto.inference.ollama;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection
-public record OllamaModelName(
-    @JsonProperty("connectionId") String connectionId,
-    @JsonProperty("modelName") String modelName) {}
