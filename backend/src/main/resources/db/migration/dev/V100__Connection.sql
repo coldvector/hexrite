c9fded1e558735e537741e1836c8da1ef49b26e3
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-04 15:08:45 -0600

    update some dev records

--- backend/src/main/resources/db/migration/dev/V100__Connection.sql
+++ backend/src/main/resources/db/migration/dev/V100__Connection.sql
@@ -13,8 +13,8 @@ INSERT INTO hexrite.connections (
 VALUES (
         '33e8015c-d306-4cf1-a983-a0c248ba2809',
         1,
-        'Ollama-1',
-        'Workstation Ollama',
+        'Ollama Work',
+        'Ollama Workstation',
         'OLLAMA',
         'http://testnet2:11434',
         '',
@@ -25,8 +25,8 @@ VALUES (
     (
         'c683af6f-7968-460d-bf97-09e9aa5c5888',
         1,
-        'Ollama-2',
-        'Homestation Ollama',
+        'Ollama Home',
+        'Ollama Homestation',
         'OLLAMA',
         'http://127.0.0.1:11434',
         '',
@@ -37,8 +37,8 @@ VALUES (
     (
         'ba907b31-9175-4ed3-ad24-f35e764145c4',
         1,
-        'Gemini API Key 1',
-        'GCP Project 111122223333',
+        'Gemini API Project',
+        'GCP Project 210308234727',
         'GEMINI',
         'https://generativelanguage.googleapis.com/v1beta',
         'XXXXXXXXXXXXXXXXXXXXXXXXXXXX',
