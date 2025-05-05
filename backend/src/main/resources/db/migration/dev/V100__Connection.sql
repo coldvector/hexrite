a4e3f98fafebd0852e20157d92001369a1ace654
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-04 19:36:18 -0600

    optlock starts from 0

--- backend/src/main/resources/db/migration/dev/V100__Connection.sql
+++ backend/src/main/resources/db/migration/dev/V100__Connection.sql
@@ -12,7 +12,7 @@ INSERT INTO hexrite.connections (
     )
 VALUES (
         '33e8015c-d306-4cf1-a983-a0c248ba2809',
-        1,
+        0,
         'Ollama Work',
         'Ollama Workstation',
         'OLLAMA',
@@ -24,7 +24,7 @@ VALUES (
     ),
     (
         'c683af6f-7968-460d-bf97-09e9aa5c5888',
-        1,
+        0,
         'Ollama Home',
         'Ollama Homestation',
         'OLLAMA',
@@ -36,7 +36,7 @@ VALUES (
     ),
     (
         'ba907b31-9175-4ed3-ad24-f35e764145c4',
-        1,
+        0,
         'Gemini API Project',
         'GCP Project 210308234727',
         'GEMINI',
