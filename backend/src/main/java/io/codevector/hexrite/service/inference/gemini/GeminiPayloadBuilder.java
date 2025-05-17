8bd25cd1bdd89b858972f912d4c6554d14fadf40
Bhavyai Gupta <zbhavyai@gmail.com>
2025-05-16 19:43:53 -0600

    add payload builder for chat with gemini

--- backend/src/main/java/io/codevector/hexrite/service/inference/gemini/GeminiPayloadBuilder.java
+++ backend/src/main/java/io/codevector/hexrite/service/inference/gemini/GeminiPayloadBuilder.java
@@ -1,8 +1,10 @@
 package io.codevector.hexrite.service.inference.gemini;
 
+import io.codevector.hexrite.entity.chat.Message;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class GeminiPayloadBuilder {
@@ -16,5 +18,23 @@ public class GeminiPayloadBuilder {
                     new JsonObject()
                         .put("role", "user")
                         .put("parts", new JsonArray().add(new JsonObject().put("text", prompt)))));
+  }
+
+  public JsonObject createPayloadChat(List<Message> messages) {
+    JsonArray payload = new JsonArray();
+    messages.forEach(m -> payload.add(messageToContent(m)));
+    return new JsonObject()
+        .put("contents", payload)
+        .put("generationConfig", createTextGenerationConfig());
+  }
+
+  public JsonObject createTextGenerationConfig() {
+    return new JsonObject().put("responseModalities", new JsonArray().add("TEXT"));
+  }
+
+  private JsonObject messageToContent(Message message) {
+    return new JsonObject()
+        .put("role", message.role)
+        .put("parts", new JsonArray().add(new JsonObject().put("text", message.content)));
   }
 }
